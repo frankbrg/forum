514e86ea73e79c4988b6148c1bde8595c5372d78
Frank Bergé <frank.brg@icloud.com>
2022-06-28 16:40:19 +0200

    :sparkles: add edit profile page

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,21 +7,21 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use App\Entity\User;
-use App\Form\RegistrationFormType;
+use App\Form\UserFormType;
 use App\Security\Authenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
-
+#[Route(path: '/authentification/', name: 'authentication_')]
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
+    #[Route(path: 'login', name: 'login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_main');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -31,11 +31,11 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
-    #[Route('/register', name: 'app_register')]
+    #[Route('register', name: 'register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, UserAuthenticatorInterface $userAuthenticator, Authenticator $authenticator): Response
     {
         $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form = $this->createForm(UserFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -60,11 +60,66 @@ class SecurityController extends AbstractController
         }
 
         return $this->render('security/register.html.twig', [
-            'registrationForm' => $form->createView(),
+            'userForm' => $form->createView(),
         ]);
     }
 
-    #[Route(path: '/logout', name: 'app_logout')]
+    #[Route('edit', name: 'edit')]
+    public function edit(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, UserAuthenticatorInterface $userAuthenticator, Authenticator $authenticator): Response
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('authentication_login');
+        }
+
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $userAuthenticator->authenticateUser(
+                $user,
+                $authenticator,
+                $request
+            );
+
+            return $this->redirectToRoute('app_main');
+        }
+
+        return $this->render('security/edit.html.twig', [
+            'userForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route(path: 'delete', name: 'delete_account')]
+    public function delete(EntityManagerInterface $entityManager): Response
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('authentication_login');
+        }
+
+        $user = $this->getUser();
+
+        $this->container->get('security.token_storage')->setToken(null);
+
+        $entityManager->remove($user);
+        $entityManager->flush();
+        
+        return $this->redirectToRoute('app_main');
+    }
+    
+    #[Route(path: 'logout', name: 'authentication_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
